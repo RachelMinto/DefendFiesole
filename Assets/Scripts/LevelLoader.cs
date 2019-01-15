92356716a5f42e6be9ae4ca314ea819656291c2e
Rachel Minto <rminto@wistia.com>
2019-01-15 09:49:45 -0500

    Initial commit and Splash + Start Screens.

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Added for scene script!
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour {
+
+    [SerializeField] int timeToWait = 3;
+    int currentSceneIndex;
+
+    // Use this for initialization
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 0) {
+            StartCoroutine(WaitForTimeAndLoadNextScene());
+        }
+	}
+
+    IEnumerator WaitForTimeAndLoadNextScene() {
+        yield return new WaitForSeconds(timeToWait);
+        LoadNextScene();
+    }
+
+    void LoadNextScene() {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
