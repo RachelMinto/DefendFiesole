246f7fc3036b429ef720df117d3207f15504fd9b
Rachel Minto <rminto@wistia.com>
2019-01-21 08:54:36 -0500

    Spawn attackersgit add .

---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour {
+    bool spawn = true;
+    [SerializeField] float minSpawnTime = 1f;
+    [SerializeField] float maxSpawnTime = 5f;
+    [SerializeField] Attacker attackerPrefab;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(SpawnEnemies());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    IEnumerator SpawnEnemies() {
+        while (spawn) {
+            yield return new WaitForSeconds(Random.Range(minSpawnTime, maxSpawnTime));
+            SpawnEnemy();
+        }
+
+    }
+
+    void SpawnEnemy() {
+        var newEnemy = Instantiate(
+            attackerPrefab,
+            transform.position,
+            Quaternion.identity
+        );
+    }
+
+}
