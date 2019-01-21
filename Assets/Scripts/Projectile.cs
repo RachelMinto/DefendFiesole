0f65d98ba3dbe55ce171e387a81246451a72e235
Rachel Minto <rminto@wistia.com>
2019-01-21 10:58:13 -0500

    Cactus shoots spinning zucchini.

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+    [SerializeField] float movementSpeed = 5f;
+    [SerializeField] float degreesRotation = -360f;
+
+	void Update () {
+        //transform.Rotate(Vector3.forward * -40);
+        //transform.Translate(Vector2.right * movementSpeed * Time.deltaTime);
+
+        transform.Translate(Vector3.right * movementSpeed * Time.deltaTime, Space.World);
+        transform.Rotate(Vector3.forward, degreesRotation * Time.deltaTime);
+	}
+}
