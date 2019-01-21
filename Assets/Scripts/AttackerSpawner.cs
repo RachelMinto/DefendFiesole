5529ed2f57d28ea5383c0bdb3a2d17538212ab63
Rachel Minto <rminto@wistia.com>
2019-01-21 09:32:09 -0500

    Set up first defender - Cactus.

--- Assets/Scripts/AttackerSpawner.cs
+++ Assets/Scripts/AttackerSpawner.cs
@@ -25,7 +25,7 @@ public class AttackerSpawner : MonoBehaviour {
     }
 
     void SpawnEnemy() {
-        var newEnemy = Instantiate(
+        Instantiate(
             attackerPrefab,
             transform.position,
             Quaternion.identity
