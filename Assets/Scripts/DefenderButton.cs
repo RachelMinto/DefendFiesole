045ed2f332343b25d90c8ec19efe57e231a25a71
Rachel Minto <rminto@wistia.com>
2019-01-23 08:44:19 -0500

    Buttons to select defenders that toggle white/grey on selection.

---
+++ Assets/Scripts/DefenderButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderButton : MonoBehaviour {
+
+    public void OnMouseDown()
+    {
+        var buttons = FindObjectsOfType<DefenderButton>();
+        foreach(DefenderButton button in buttons) {
+            button.GetComponent<SpriteRenderer>().color = new Color32(53, 53, 53, 255);
+        }
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+}
