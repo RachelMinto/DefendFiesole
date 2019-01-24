1bcb3193932a23ac94073028f21f1f4fcb0a8627
Rachel Minto <rminto@wistia.com>
2019-01-24 08:29:45 -0500

    Basic currency structure.

---
+++ Assets/Scripts/StarsDisplay.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// Remember to use UI!
+using UnityEngine.UI;
+
+public class StarsDisplay : MonoBehaviour {
+    [SerializeField] int stars = 100;
+    Text starText;
+
+	// Use this for initialization
+	void Start () {
+        starText = GetComponent<Text>();
+        UpdateDisplay();
+	}
+	
+	// Update is called once per frame
+	private void UpdateDisplay () {
+        starText.text = stars.ToString();
+	}
+
+    public void AddStars(int amount) {
+        stars += amount;
+        UpdateDisplay();
+    }
+
+    public void SpendStars(int amount)
+    {
+        if (stars >= amount) {
+            stars -= amount;
+            UpdateDisplay();            
+        }
+    }
+}
