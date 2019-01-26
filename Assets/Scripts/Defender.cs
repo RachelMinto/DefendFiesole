236bdc6599f66c78744eeaa309e200ee4c101ef1
Rachel Minto <rminto@wistia.com>
2019-01-26 13:01:13 -0500

    Add star to trophy prefab and AddStars animation.

--- Assets/Scripts/Defender.cs
+++ Assets/Scripts/Defender.cs
@@ -4,4 +4,8 @@ using UnityEngine;
 
 public class Defender : MonoBehaviour {
     [SerializeField] int starCost = 100;
+
+    public void AddStars(int amount) {
+        FindObjectOfType<StarsDisplay>().AddStars(amount);
+    }
 }
