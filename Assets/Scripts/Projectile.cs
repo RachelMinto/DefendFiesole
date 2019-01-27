43c4ddb96434f21c18d2cb537c9cfe2c65b14e92
Rachel Minto <rminto@wistia.com>
2019-01-27 11:06:11 -0500

    Organize prefabs; make new Axe projectile.

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -7,9 +7,6 @@ public class Projectile : MonoBehaviour {
     [SerializeField] float degreesRotation = -360f;
 
 	void Update () {
-        //transform.Rotate(Vector3.forward * -40);
-        //transform.Translate(Vector2.right * movementSpeed * Time.deltaTime);
-
         transform.Translate(Vector3.right * movementSpeed * Time.deltaTime, Space.World);
         transform.Rotate(Vector3.forward, degreesRotation * Time.deltaTime);
 	}
