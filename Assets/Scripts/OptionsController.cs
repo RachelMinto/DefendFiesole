223a20800be978692492c636df70d9645aa3cea0
Rachel Minto <rminto@wistia.com>
2019-02-06 08:33:40 -0500

    Game music and options for music volume.

---
+++ Assets/Scripts/OptionsController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsController : MonoBehaviour {
+    [SerializeField] Slider volumeSlider;
+
+	void Start () {
+        volumeSlider.value = PlayerPrefsController.GetMasterVolume();	
+	}
+
+    private void Update()
+    {
+        MusicPlayer musicPlayer = GetMusicPlayer();
+        if (musicPlayer)
+        {
+            musicPlayer.SetVolume(volumeSlider.value);
+        }
+        else
+        {
+            Debug.LogWarning("No music player. Did you start from Splash Screen?");
+        }
+    }
+
+    public void SaveAndExit() {
+        PlayerPrefsController.SetMasterVolume(volumeSlider.value);
+        FindObjectOfType<LevelLoader>().LoadStartScene();
+    }
+
+    public void SetToDefaultValues() {
+        float defaultVolume = PlayerPrefsController.GetDefaultVolume();
+        volumeSlider.value = defaultVolume;
+    }
+
+    private MusicPlayer GetMusicPlayer() {
+        return FindObjectOfType<MusicPlayer>();
+    }
+}
---
+++ Assets/Scripts/PlayerPrefsController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPrefsController : MonoBehaviour {
+
+    const string MASTER_VOLUME_KEY = "master volume"; 
+    const float MIN_VOLUME = 0f; 
+    const float MAX_VOLUME = 1f; 
+    const string DIFFICULTY_KEY = "difficulty"; 
+    static float defaultVolume = 0.8f;
+
+    public void Start()
+    {
+        PlayerPrefs.SetFloat(MASTER_VOLUME_KEY, defaultVolume);
+    }
+
+    public static void SetMasterVolume(float volume)
+    {
+        if (volume >= MIN_VOLUME && volume <= MAX_VOLUME) {
+            PlayerPrefs.SetFloat(MASTER_VOLUME_KEY, volume);
+        }
+        else 
+        {
+            Debug.LogError("Master volume is out of range");
+        }
+    }
+
+    public static float GetDefaultVolume() 
+    {
+        return defaultVolume;
+    }
+
+    public static float GetMasterVolume()
+    {
+        return PlayerPrefs.GetFloat(MASTER_VOLUME_KEY);
+    }
+}
